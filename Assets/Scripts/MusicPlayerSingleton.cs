0afd70faeb6e6f9f7e39ceb543bb2a1830cbb2e3
Daron <daronlin303@gmail.com>
2015-04-26 15:06:20 -0400

    implemenet bg music and game music

--- Assets/Scripts/MusicPlayerSingleton.cs
+++ Assets/Scripts/MusicPlayerSingleton.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MusicPlayerSIngleton : MonoBehaviour {
-	public static MusicPlayerSIngleton instance;
+public class MusicPlayerSingleton : MonoBehaviour {
+	public static MusicPlayerSingleton instance;
 	public AudioClip cutSceneMusic;
 	public AudioClip gameSceneMusic;
 
+	AudioSource source;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		if (instance != null && instance != this) {
 			Destroy (this.gameObject);
 		} else {
@@ -15,11 +16,44 @@ public class MusicPlayerSIngleton : MonoBehaviour {
 			DontDestroyOnLoad(this.gameObject);
 		}
 
+		source = GetComponent<AudioSource> ();
+		Debug.Log ("reached the end of awake");
 	}
 
-	void OnLevelWasLoaded(int level) {
-
+	void Start() {
+		playCutSceneMusic ();
 	}
+
+	public void playGameMusic() {
+		playMusic (gameSceneMusic);
+	}
+
+	public void playCutSceneMusic() {
+		playMusic (cutSceneMusic);
+	}
+
+	public void playMusic(AudioClip clip) {
+		if (source != null) {
+			source.Stop ();
+			source.clip = clip;
+			source.Play ();
+		}
+	}
+
+	public void stopMusic() {
+		if (source != null) {
+			source.Stop ();
+		}
+	}
+	
+	public static MusicPlayerSingleton GetInstance() {
+		return instance;
+	}
+
+	public static MusicPlayerSingleton Instance {
+		get { return instance; }
+	}
+	
 	
 	// Update is called once per frame
 	void Update () {
