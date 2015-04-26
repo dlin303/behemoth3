dbfaeb0bdc5f10a25fa65d61f0e26be429b0d6f4
Daron <daronlin303@gmail.com>
2015-04-26 14:24:11 -0400

    modify skip game text

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -4,12 +4,31 @@ using System.Collections;
 public class SceneManager : MonoBehaviour {
 	// Use this for initialization
 	public string nextLevel;
+	public float sceneTime = 3f;
+	public bool isTitleScreen = false;
+
+	float startTime;
+
+	void Start() {
+		startTime = Time.time;
+	}
 
 	// Update is called once per frame
 	void Update () {
-		float inputX = Input.GetAxis("Horizontal");
-		if (inputX > 0 && !string.IsNullOrEmpty(nextLevel)) {
-			Application.LoadLevel(nextLevel);
+		if (!isTitleScreen) {
+			float inputX = Input.GetAxis ("Horizontal");
+			if (Time.time - startTime > sceneTime || (inputX > 0 && !string.IsNullOrEmpty (nextLevel))) {
+				Application.LoadLevel (nextLevel);
+			}
+
+		} else {
+			if (Input.GetKeyDown (KeyCode.Return)) {
+				Application.LoadLevel (nextLevel);
+			}
+		}
+
+		if (Input.GetKeyDown ("space")) {
+			Application.LoadLevel ("GameInstructions");
 		}
 	}
 }
